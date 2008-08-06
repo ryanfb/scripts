40dc090a70409b13713b0ede130dd59c919a941f
Ryan Baumann <ryan.baumann@gmail.com>
2008-08-06 12:38:43 -0400

    Initial commit of a few existing scripts

---
+++ 3d/vtx2ply.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+@option_rgb = false
+
+opts = OptionParser.new
+opts.on('-c', '--color') { @option_rgb = true }
+opts.on('-f', '--faces file') { |f| @option_faces = f }
+
+opts.parse!(ARGV)
+
+vtx = File.readlines(ARGV[0].to_s)
+ply = File.new(ARGV[1].to_s, "w")
+
+# Strip comments
+vtx.delete_if { |v| v =~ /^\#/ }
+
+ply.puts "ply"
+ply.puts "format ascii 1.0"
+ply.puts "element vertex #{vtx.length}"
+
+ply.puts <<EOH
+property float x
+property float y
+property float z
+property float nx
+property float ny
+property float nz
+EOH
+
+if @option_rgb
+	['red','green','blue'].each do |c|
+		ply.puts "property uchar #{c}"
+	end
+end
+
+if @option_faces
+	faces = File.readlines(@option_faces)
+	num_faces = faces.length
+else
+	num_faces = 0
+end
+
+ply.puts "element face #{num_faces}"
+ply.puts "property list uchar int vertex_indices"
+ply.puts "end_header"
+
+ply.puts vtx
+
+if faces
+	ply.puts faces
+end
---
+++ video/join-video.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+include FileUtils::Verbose
+
+if ARGV.length == 1
+	chdir(File.expand_path(ARGV[0]) + "/")
+end
+
+mediafiles = Dir['**/*.avi'] + Dir['**/*.mpg']
+mediafiles.reject! {|a| a =~ /sample/}
+mediafiles.sort!
+
+if mediafiles.length > 1
+	output = mediafiles[0][0..-5] + "-joined" + mediafiles[0][-4..-1]
+	mediafiles.map! {|i| "\"#{i}\""}
+	input = mediafiles.join(" ") 
+	puts "Input: #{input}"
+	puts "Output: #{output}"
+	
+	command = "mencoder -forceidx -noodml -oac copy -ovc copy -o \"#{output}\" #{input}"
+	
+	system(command)
+end
