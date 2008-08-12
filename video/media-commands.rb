c72c6baaf0a599c42681c687f9e9abe6c193cb3e
Ryan Baumann <ryan.baumann@gmail.com>
2008-08-12 18:15:42 -0400

    Add media commands lib

---
+++ video/media-commands.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+class MediaFormatException < StandardError
+end
+
+def execute_mencoder(command)
+  progress = nil
+  IO.popen(command) do |pipe|
+    pipe.each("\r") do |line|
+      if line =~ /Pos:[^(]*\(\s*(\d+)%\)/
+        p = $1.to_i
+        p = 100 if p > 100
+        if progress != p
+          progress = p
+          print "PROGRESS: #{progress}\n"
+          $defout.flush
+        end
+      end
+    end
+  end
+  raise MediaFormatException if $?.exitstatus != 0
+end
+
+def execute_ffmpeg(command)
+  progress = nil
+  IO.popen(command) do |pipe|
+    pipe.each("\r") do |line|
+      if line =~ /Duration: (\d{2}):(\d{2}):(\d{2}).(\d{1})/
+        duration = (($1.to_i * 60 + $2.to_i) * 60 + $3.to_i) * 10 + $4.to_i
+      end
+      if line =~ /time=(\d+).(\d+)/
+        if not duration.nil? and duration != 0
+          p = ($1.to_i * 10 + $2.to_i) * 100 / duration
+        else
+          p = 0
+        end
+        p = 100 if p > 100
+        if progress != p
+          progress = p
+          print "PROGRESS: #{progress}\n"
+          $defout.flush
+        end
+      end
+    end
+  end
+  raise MediaFormatException if $?.exitstatus != 0
+end
+
+def safe_execute
+  yield
+rescue
+  print "ERROR\n"
+  exit 1
+end
