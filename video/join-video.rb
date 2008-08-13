ab7781a097aa9b55bbd7ffe5618eb7974564c1ed
Ryan Baumann <ryan.baumann@gmail.com>
2008-08-12 20:56:24 -0400

    Take multiple directories as arguments

--- video/join-video.rb
+++ video/join-video.rb
@@ -7,22 +7,30 @@ require 'fileutils'
 
 include FileUtils::Verbose
 
-if ARGV.length == 1
-	chdir(File.expand_path(ARGV[0]) + "/")
+def join_all_videos(dir = {})
+	if dir != {}
+		chdir(dir)
+	end
+
+	mediafiles = Dir['**/*.avi'] + Dir['**/*.mpg']
+	mediafiles.reject! {|a| a =~ /sample/}
+	mediafiles.sort!
+
+	if mediafiles.length > 1
+		output = mediafiles[0][0..-5] + "-joined" + mediafiles[0][-4..-1]
+		mediafiles.map! {|i| "\"#{i}\""}
+		input = mediafiles.join(" ") 
+		puts "Input: #{input}"
+		puts "Output: #{output}"
+		
+		command = "mencoder -forceidx -noodml -oac copy -ovc copy -o \"#{output}\" #{input}"
+		
+		system(command)
+	end
 end
 
-mediafiles = Dir['**/*.avi'] + Dir['**/*.mpg']
-mediafiles.reject! {|a| a =~ /sample/}
-mediafiles.sort!
-
-if mediafiles.length > 1
-	output = mediafiles[0][0..-5] + "-joined" + mediafiles[0][-4..-1]
-	mediafiles.map! {|i| "\"#{i}\""}
-	input = mediafiles.join(" ") 
-	puts "Input: #{input}"
-	puts "Output: #{output}"
-	
-	command = "mencoder -forceidx -noodml -oac copy -ovc copy -o \"#{output}\" #{input}"
-	
-	system(command)
+if ARGV.length > 0
+	ARGV.each {|dir| join_all_videos(expand_path(dir) + "/")}
+else
+	join_all_videos
 end
