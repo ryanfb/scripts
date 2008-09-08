22862f4cb3d6675333a7bdbd6f6ffccd30259e7b
Ryan Baumann <ryan.baumann@gmail.com>
2008-09-08 09:29:50 -0400

    Use the correct Mac Icon filename

--- files/mess.rb
+++ files/mess.rb
@@ -26,9 +26,9 @@ else
     warn "Created messdir #{current}"
 
     # Set a snazzy icon on Mac OS X
-    iconfile = File.join(MESSDIR, "icon")
+    iconfile = File.join(MESSDIR, "Icon\r")
     if File.exist?(iconfile)
-      FU.cp(iconfile, File.join(current,"Icon?"))
+      FU.cp(iconfile, File.join(current,"Icon\r"))
       system("/Developer/Tools/SetFile -a C #{current}")
       warn "Set icon"
     end
