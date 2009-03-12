200feabbf4ceb8c11452b85b63d0539d19b82d64
Ryan Baumann <ryan.baumann@gmail.com>
2009-03-12 11:26:23 -0400

    add Downloads directory

--- files/mess.rb
+++ files/mess.rb
@@ -20,10 +20,15 @@ else
   d = Date.today
   current = File.join(MESSDIR, d.year.to_s, "%02d" % d.cweek)
   current_link = File.join(MESSDIR, "current")
+	downloads = File.join(current, "Downloads")
+	downloads_link = File.join(MESSDIR, "Downloads")
 
   unless File.directory? current
     FU.mkdir_p current
     warn "Created messdir #{current}"
+
+		FU.mkdir_p downloads
+		warn "Created Downloads dir #{downloads}"
 
     # Set a snazzy icon on Mac OS X
     iconfile = File.join(MESSDIR, "Icon\r")
@@ -34,13 +39,15 @@ else
     end
   end
 
-  if File.exist?(current_link) && !File.symlink?(current_link)
-    warn "`#{current_link}' is not a symlink, something is wrong."
-  else
-    if File.expand_path(current_link) != File.expand_path(current)
-      FU.rm_f current_link
-      FU.ln_s current, current_link
-    end
-  end
-  puts current_link
+	[[current_link, current], [downloads_link, downloads]].each do |mess_link_pair|
+		if File.exist?(mess_link_pair[0]) && !File.symlink?(mess_link_pair[0])
+			warn "`#{mess_link_pair[0]}' is not a symlink, something is wrong."
+		else
+			if File.expand_path(mess_link_pair[0]) != File.expand_path(mess_link_pair[1])
+				FU.rm_f mess_link_pair[0]
+				FU.ln_s mess_link_pair[1], mess_link_pair[0]
+			end
+		end
+	end
+	puts current_link
 end
